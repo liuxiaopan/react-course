53c594cef15a9f9f9b57a46846a375ab1d0d501e
Linda <Linda@Linda.local>
2016-08-10 13:08:00 +0800

    babel ¿Îʱ3

---
+++ build/index.js
@@ -0,0 +1,17 @@
+'use strict';
+
+var _from = require('babel-runtime/core-js/array/from');
+
+var _from2 = _interopRequireDefault(_from);
+
+require('babel-pollyfill');
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+console.log((0, _from2.default)('sssssxx'));
+
+var sum = function sum(a, b) {
+  return a + b;
+};
+var c = sum(2, 222);
+console.console.log(c);
---
+++ src/index.js
@@ -0,0 +1,7 @@
+import 'babel-pollyfill';
+
+console.log(Array.from('sssssxx'));
+
+const sum=(a,b)=>a+b;
+let c=sum(2,222);
+console.console.log(c);
